c5ed8a598e2717bc578e39b6d65583f74a9dc3c8
selenefregosi-noaa <selene.fregosi@noaa.gov>
2024-12-04 11:42:05 -0800

    started workflow

---
+++ MATLAB/workflow_processWISPR_CalCurCEAS2024.m
@@ -0,0 +1,44 @@
+% WORKFLOW_PROCESSWISPR_CALCURCEAS2024.M
+%	Workflow for processing raw WISPR data into .flac, CalCurCEAS 2024
+%
+%	Description:
+%		Detailed description here, please
+%
+%	Notes
+%
+%	See also
+%
+%
+%	Authors:
+%		S. Fregosi <selene.fregosi@gmail.com> <https://github.com/sfregosi>
+%
+%	FirstVersion: 	04 December 2024
+%	Updated:
+%
+%	Created with MATLAB ver.: 24.2.0.2740171 (R2024b) Update 1
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+addpath(genpath('C:\Users\Selene.Fregosi\Documents\MATLAB\agate'))
+path_repo = 'C:\Users\Selene.Fregosi\Documents\GitHub\glider-CalCurCEAS';
+
+missionStrs = {'sg639_CalCurCEAS_Sep2024';
+	'sg679_CalCurCEAS_Aug2024';
+    'sg680_CalCurCEAS_Sep2024'};
+
+mtpNum = 1; % mission to process - UPDATE THIS TO RUN THROUGH EACH GLIDER
+mtp = missionStrs{mtpNum};
+
+% initialize agate
+% make sure configuration file now has updated WISPR Settings section
+% (not required during mission so may not be set yet)
+CONFIG = agate(fullfile(path_repo, 'MATLAB', 'fregosi_config_files', ...
+    ['agate_config_' mtp '.cnf']));
+
+%%
+
+
+% update restartDir if needed after errors
+CONFIG.pm.restartDir = 'pm0045b';  % re-start conversion at this directory
+
+% convert!
+convertPmarFun(CONFIG)
