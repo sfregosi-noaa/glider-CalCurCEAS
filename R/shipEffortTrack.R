5803a33f5519590fc50736defbba216ffad496a7
selenefregosi-noaa <selene.fregosi@noaa.gov>
2025-01-10 10:33:09 -0800

    Create shipEffortTrack.R

---
+++ R/shipEffortTrack.R
@@ -0,0 +1,7 @@
+#' ---------------------------
+#' title: Extract CalCurCEAS2024 Ship Track
+#' authors: Selene Fregosi
+#' purpose: extract on effort ship tracks for CalCurCEAS 2024 with the intent to
+#' plot the ship tracks with the DASBRs and gliders
+#' The DASALL2024 file is from January 2025 and is not the final QA/QC'd version
+#' ---------------------------
