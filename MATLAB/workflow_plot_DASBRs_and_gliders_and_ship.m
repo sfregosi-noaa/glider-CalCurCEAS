6102e62b341deb3ecf887eee1140e8f5c7829eac
selenefregosi-noaa <selene.fregosi@noaa.gov>
2025-01-14 12:32:32 -0800

    added colorbar and fixed timezone

--- MATLAB/workflow_plot_DASBRs_and_gliders_and_ship.m
+++ MATLAB/workflow_plot_DASBRs_and_gliders_and_ship.m
@@ -1,5 +1,5 @@
-% WORKFLOW_PLOT_DASBRS_AND_GLIDERS.M
-%	Script to make a map of the DASBRs and the gliders, colorized by time
+% WORKFLOW_PLOT_DASBRS_AND_GLIDERS_AND_SHIP.M
+%	Script to make a map of the DASBRs, gliders and ship colored by time
 %
 %	Description:
 %
@@ -34,13 +34,14 @@ cnfFile = fullfile(path_repo, 'MATLAB', 'fregosi_config_files', ...
 CONFIG = agate(cnfFile);
 
 % cannot use createBasemap because axes need to NOT be a map axis to
-% colorize the trackline by a z value (time). But pull pieces from it
+% colorize the trackline by a z value (time). But pull pieces from that
+% function to assemble fig
 
 % get axis limits
 % CONFIG.map.latLim;
 % CONFIG.map.lonLim;
 latLim = [40.25 45];
-lonLim = [-129 -123.2];
+lonLim = [-129 -123.1];
 
 % rounded limits (for bathy loading) to make sure all right sizes
 latLimWide = [40 48];
@@ -49,21 +50,21 @@ lonLimWide = [-130 -122];
 % load bathy/contour data
 CONFIG.map.bathyFile = ['C:\Users\Selene.Fregosi\Documents\GIS\etopo\' ...
     'ETOPO2022_ice_15arcsec_OR_wide.tiff'];
-	[Z, refvec] = readgeoraster(CONFIG.map.bathyFile, 'OutputType', 'double', ...
-		'CoordinateSystemType', 'geographic');
-	[Z, refvec] = geocrop(Z, refvec, latLimWide, lonLimWide);
+[Z, refvec] = readgeoraster(CONFIG.map.bathyFile, 'OutputType', 'double', ...
+    'CoordinateSystemType', 'geographic');
+[Z, refvec] = geocrop(Z, refvec, latLimWide, lonLimWide);
 % this is upsidedown for some reason. flip it
 Z = flipud(Z);
 
-	Z(Z >= 10) = 100;
-	% define axes limits (can't use refvec in non-map axes)
-	Zy = [latLimWide(1):1/60/4:latLimWide(2)];
-	Zx = [lonLimWide(1):1/60/4:lonLimWide(2)];
+Z(Z >= 10) = 100;
+% define axes limits (can't use refvec in non-map axes)
+Zy = [latLimWide(1):1/60/4:latLimWide(2)];
+Zx = [lonLimWide(1):1/60/4:lonLimWide(2)];
 
-	% add contours (modify these!)
-	levels = [-6000:500:50];
+% add contours (modify these!)
+levels = [-6000:500:50];
 
-	% plot
+% plot
 figure(21)
 clf
 
@@ -74,24 +75,24 @@ ax1 = axes;
 
 %% add glider tracks
 ax2 = axes;
-hold on; 
+hold on;
 
 % sg639
 % targetsSimple = fullfile(CONFIG.path.mission, 'targets_A_Nearshore_2024-09-30');
 targetsSimple = fullfile(path_repo, 'mission_planning', 'targets_A_Nearshore_2024-09-30');
 [targets, ~] = readTargetsFile(CONFIG, targetsSimple);
 h(1) = plot(targets.lon, targets.lat, 'Marker', 'o', 'MarkerSize', 4, ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0], ...
-	'HandleVisibility', 'off');
+    'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0], ...
+    'HandleVisibility', 'off');
 text(targets.lon-0.1, targets.lat+0.1, targets.name, 'FontSize', 6)
 % pull properly formatted locations for pp
 load(fullfile(CONFIG.path.mission, 'flightStatus', ['diveTracking_' ...
-	CONFIG.glider '.mat']));
+    CONFIG.glider '.mat']));
 surfSimp = ppToGPSSurf(CONFIG, pp);
 writetable(surfSimp, fullfile(path_repo, 'maps', 'sg639_CalCurCEAS_Sep24_simpleSurfaceGPS.csv'))
 h(2) = color_line3(surfSimp.longitude, surfSimp.latitude, ...
-	surfSimp.time_UTC, surfSimp.time_UTC, 'LineWidth', 1.5, ...
-	'DisplayName', CONFIG.glider);
+    surfSimp.time_UTC, surfSimp.time_UTC, 'LineWidth', 1.5, ...
+    'DisplayName', CONFIG.glider);
 % colormap(hot);
 
 % sg680
@@ -102,17 +103,17 @@ CONFIG = agate(cnfFile);
 targetsSimple = fullfile(path_repo, 'mission_planning', 'targets_B_Nearshore_2024-10-14');
 [targets, ~] = readTargetsFile(CONFIG, targetsSimple);
 h(3) = plot(targets.lon, targets.lat, 'Marker', 'o', 'MarkerSize', 4, ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0], ...
-	'HandleVisibility', 'off');
+    'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0], ...
+    'HandleVisibility', 'off');
 text(targets.lon-0.1, targets.lat+0.1, targets.name, 'FontSize', 6)
 % pull properly formatted locations for pp
 load(fullfile(CONFIG.path.mission, 'flightStatus', ['diveTracking_' ...
-	CONFIG.glider '.mat']));
+    CONFIG.glider '.mat']));
 surfSimp = ppToGPSSurf(CONFIG, pp);
 writetable(surfSimp, fullfile(path_repo, 'maps', 'sg680_CalCurCEAS_Aug24_simpleSurfaceGPS.csv'))
 h(4) = color_line3(surfSimp.longitude, surfSimp.latitude, ...
-	surfSimp.time_UTC, surfSimp.time_UTC, 'LineWidth', 1.5, ...
-	'DisplayName', CONFIG.glider);
+    surfSimp.time_UTC, surfSimp.time_UTC, 'LineWidth', 1.5, ...
+    'DisplayName', CONFIG.glider);
 % colormap(hot);
 
 % sg679
@@ -123,22 +124,26 @@ CONFIG = agate(cnfFile);
 targetsSimple = fullfile(path_repo, 'mission_planning', 'targets_C_Offshore_2024-08-15');
 [targets, ~] = readTargetsFile(CONFIG, targetsSimple);
 h(5) = plot(targets.lon, targets.lat, 'Marker', 'o', 'MarkerSize', 4, ...
-	'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0], ...
-	'HandleVisibility', 'off');
+    'MarkerEdgeColor', [0 0 0], 'MarkerFaceColor', [0 0 0], 'Color', [0 0 0], ...
+    'HandleVisibility', 'off');
 text(targets.lon-0.1, targets.lat+0.1, targets.name, 'FontSize', 6)
 % pull properly formatted locations for pp
 load(fullfile(CONFIG.path.mission, 'flightStatus', ['diveTracking_' ...
-	CONFIG.glider '.mat']));
+    CONFIG.glider '.mat']));
 surfSimp = ppToGPSSurf(CONFIG, pp);
 % remove the bad fixes for SG679
 surfSimp(244:245,:) = [];
 writetable(surfSimp, fullfile(path_repo, 'maps', 'sg679_CalCurCEAS_Sep24_simpleSurfaceGPS.csv'))
 h(6) = color_line3(surfSimp.longitude, surfSimp.latitude, ...
-	surfSimp.time_UTC, surfSimp.time_UTC, 'LineWidth', 1.5, ...
-	'DisplayName', CONFIG.glider);
+    surfSimp.time_UTC, surfSimp.time_UTC, 'LineWidth', 1.5, ...
+    'DisplayName', CONFIG.glider);
 % colormap(hot);
 
-hCount = 6; % for keeping track of h objects 
+% get min/max dates from SG679 (out first)
+minDatenum = min(surfSimp.time_UTC);
+maxDatenum = max(surfSimp.time_UTC);
+
+hCount = 6; % for keeping track of h objects
 %% add DASBR tracks
 % dasbrs = kml2struct(fullfile(path_repo, 'DASBRs', ...
 % 	'CalCurCEAS_2024_DASBR_and_effort_thru_Sep26.kml'));
@@ -151,46 +156,73 @@ for f = [3, 5:15, 18]
     hCount = hCount + 1;
     dasbr = readtable(fullfile(dasbrList(f).folder, dasbrList(f).name));
     dasbr.datenum = datenum(dasbr.UTC);
-	% h(6+f) = plot(dasbr.Longitude, dasbr.Latitude, 'Color', 'white', ...
-	% 	'LineWidth', 1.5, 'DisplayName', 'dasbr');
+    % h(6+f) = plot(dasbr.Longitude, dasbr.Latitude, 'Color', 'white', ...
+    % 	'LineWidth', 1.5, 'DisplayName', 'dasbr');
     h(hCount) = color_line3(dasbr.Longitude, dasbr.Latitude, ...
-	dasbr.datenum, dasbr.datenum, 'LineWidth', 1.5, ...
-	'DisplayName', ['dasbr' num2str(f)]);
+        dasbr.datenum, dasbr.datenum, 'LineWidth', 1.5, ...
+        'DisplayName', ['dasbr' num2str(f)]);
 
     text(dasbr.Longitude(1)-0.05, dasbr.Latitude(1)+0.05, ['d' num2str(f)], ...
         'FontSize', 10, 'Color', 'white')
     text(dasbr.Longitude(end)-0.05, dasbr.Latitude(end)+0.05, ['d' num2str(f)], ...
         'FontSize', 10, 'Color', 'white')
-	% dasbrs(f).Number = f;
-	% h(6+f) = plot(dasbrs(f).Lon, dasbrs(f).Lat, 'Color', 'white', ...
-	% 	'LineWidth', 1.5, 'DisplayName', dasbrs(f).Name);
+    % dasbrs(f).Number = f;
+    % h(6+f) = plot(dasbrs(f).Lon, dasbrs(f).Lat, 'Color', 'white', ...
+    % 	'LineWidth', 1.5, 'DisplayName', dasbrs(f).Name);
+
+    % check date min/max against each dasbr
+    if min(dasbr.datenum) < minDatenum
+        minDatenum = min(dasbr.datenum);
+    end
+    if max(dasbr.datenum) > maxDatenum
+        maxDatenum = max(dasbr.datenum);
+    end
 end
 
 %% add the ship
- ship = readtable(fullfile(path_repo, 'secret', 'ship_effort.csv'));
- ship.datenum = datenum(ship.DateTime);
- 
- % trim to just the area we want (otherwise color axis will be off)
+ship = readtable(fullfile(path_repo, 'secret', 'ship_effort.csv'));
+ship.datenum = datenum(ship.DateTime_UTC);
+
+% trim to just the area we want (otherwise color axis will be off)
 trimIdx = find(ship.Lat >= latLim(1) & ship.Lat <= latLim(2) & ...
     ship.Lon >= lonLim(1) & ship.Lon <= lonLim(2));
 % trimIdx = unique([latIdx; lonIdx]);
 shipTrim = ship(trimIdx,:);
 
- % loop through each segment separately so they aren't connected
- for f = 1:max(shipTrim.SegID)
-     shipSeg = shipTrim(shipTrim.SegID == f,:);
- hCount = hCount + 1;
- h(hCount) = color_line3(shipSeg.Lon, shipSeg.Lat, shipSeg.datenum, ...
-     shipSeg.datenum, 'LineWidth', 1.5, 'DisplayName', 'ship');
- end
+% % loop through each segment separately so they aren't connected
+% for f = 1:max(shipTrim.SegID)
+%     shipSeg = shipTrim(shipTrim.SegID == f,:);
+%     hCount = hCount + 1;
+%     % first plot as white slightly thicker
+%     line(shipSeg.Lon, shipSeg.Lat, 'Color', 'black', 'LineWidth', 2);
+%     % then plot colorized
+%     h(hCount) = color_line3(shipSeg.Lon, shipSeg.Lat, shipSeg.datenum, ...
+%         shipSeg.datenum, 'LineWidth', 1, 'DisplayName', 'ship');
+% end
+
+% loop through each segment separately so they aren't connected
+for f = 1:max(shipTrim.SegID)
+    shipSeg = shipTrim(shipTrim.SegID == f,:);
+    hCount = hCount + 1;
+    h(hCount) = color_line3(shipSeg.Lon, shipSeg.Lat, shipSeg.datenum, ...
+        shipSeg.datenum, 'LineWidth', 1.5, 'DisplayName', 'ship');
+end
+
+% check min/max dates
+if min(shipTrim.datenum) < minDatenum
+    minDatenum = min(shipTrim.datenum);
+end
+if max(shipTrim.datenum) > maxDatenum
+    maxDatenum = max(shipTrim.datenum);
+end
 
 %%  add some other labels
 % add newport label
-scatter(-124.05, 44.64, 200, 'white', 'p', 'filled', 'MarkerEdgeColor', 'black');
-text(-124.05, 44.48, 'Newport, OR', 'FontSize', 10, 'Color', 'white');
+scatter(-124.05, 44.64, 300, 'white', 'p', 'filled', 'MarkerEdgeColor', 'black');
+text(-124.05, 44.48, 'Newport, OR', 'FontSize', 12, 'Color', 'white');
 % add Eureka label
 scatter(-124.16, 40.8, 300, 'white', 'p', 'filled', 'MarkerEdgeColor', 'black');
-text(-124.02, 40.8, 'Eureka, CA', 'FontSize', 10, 'Color', 'white');
+text(-124.02, 40.8, 'Eureka, CA', 'FontSize', 12, 'Color', 'white');
 
 %% link the axes and turn off top layer
 linkaxes([ax1 ax2])
@@ -209,19 +241,32 @@ colormap(ax2,'jet')
 
 xlim(lonLim)
 ylim(latLim)
-% set the axis ratio. 
-dLon = 111*cosd(40); 
+% set the axis ratio.
+dLon = 111*cosd(40);
 ratio = dLon/111;
 pbaspect(ax1, [ratio 1 1])
 pbaspect(ax2, [ratio 1 1])
-% want to add colorbar but it gets written on top of the bathy map...
-% and labels are bad
-% colorbar(ax2, 'south')
+
+% add colorbar
+% Position has to be manually set so first set size then cb posit
+set(gcf, 'Position', [40 50 920 940]);
+cb2 = colorbar(ax2, 'Position', [.85 .11 .04 .815]);
+% set up ticks/labels to be readable
+colTicks = linspace(minDatenum, maxDatenum, 10); % 10 ticks
+colTickLabels = datestr(colTicks, 'mmm dd');
+cb2.Ticks = colTicks;
+cb2.TickLabels = colTickLabels;
+cb2.Label.String = 'date';
+cb2.Direction = 'reverse';
+% cb2.Label.FontSize = 14;
+% cb2.FontSize = 14;
 
 %% Save all glider map
 
+% set position/size
+set(gcf, 'Position', [40 50 920 940]);
 exportgraphics(gcf, fullfile(path_repo, 'maps', 'DASBRs_and_gliders_and_ship_coloredByTime.png'), ...
-	'Resolution', 300);
+    'Resolution', 300);
 
 
 
